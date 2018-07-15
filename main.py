3e113c6707bb4bd43f118f6c7ee2b2e35eb627dd
Colin <ragespectate@gmail.com>
2018-07-14 22:59:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def get_podcast_file_url(pocketcasts_custom_url):
 def download_podcast(url):
     urllib.request.urlretrieve(url, file_name)
 
-
 def download_podcast(url):
     urllib.request.urlretrieve(url, file_name)
 
@@ -30,23 +29,19 @@ def get_clip(startTime,endTime,cntr):
     file_clip = cntr + '-extract.mp3'
     return file_clip
 
-
 def get_transcript(clip):
     data = open(clip, 'rb').read()
-
     key = config.key
     headers = {'Accept':'application/json;text/xml',
                'Ocp-Apim-Subscription-Key' : key,
                'Host':'speech.platform.bing.com',
                'Content-type' : 'audio/mpeg; codec=audio/pcm; samplerate=16000'}
-
     r = requests.post("https://speech.platform.bing.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US&format=simple",
                       headers=headers,
                       data=data)
     return r
 
 if __name__ == "__main__":
-    
     pcast_url = config.test_url
     url = get_podcast_file_url(pcast_url)
     start_seconds = int(re.findall('t=\d*', pcast_url)[0][2:])
