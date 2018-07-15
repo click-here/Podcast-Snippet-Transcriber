7d544f62e44f74c61252c72009e794d333c0a44a
click-here <ragespectate@gmail.com>
2018-07-14 21:37:08 -0400

    batch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import urllib.request
 import os
 from pydub import AudioSegment
+from pydub.silence import detect_silence
 import requests
 import json
 import config
@@ -9,20 +10,14 @@ import config
 url = config.test_url
 
 
-startMin = 2
-startSec = 29
-file_name = os.path.basename(url)
-startTime = startMin*60*1000+startSec*1000
-endTime = startTime + 15000
-
 def download_podcast(url):
     urllib.request.urlretrieve(url, file_name)
 
-def get_clip():
+def get_clip(startTime,endTime,cntr):
     clip = AudioSegment.from_mp3(os.path.join(os.getcwd(), file_name))
     extract = clip[startTime:endTime]
-    extract.export(file_name+'-extract.mp3', format="wav")
-    file_clip = file_name + '-extract.mp3'
+    extract.export(cntr+'-extract.mp3', format="wav")
+    file_clip = cntr + '-extract.mp3'
     return file_clip
 
 
@@ -35,14 +30,31 @@ def get_transcript(clip):
                'Host':'speech.platform.bing.com',
                'Content-type' : 'audio/mpeg; codec=audio/pcm; samplerate=16000'}
 
-    r = requests.post("https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-US&format=detailed",
+    r = requests.post("https://speech.platform.bing.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US&format=simple",
                       headers=headers,
                       data=data)
     return r
 
+file_name = os.path.basename(url)
+clip_size = 8000
+
+startMin = 26
+startSec = 57
+
+startTime = (startMin*60*1000+startSec*1000) - clip_size
+endTime = startTime + (clip_size*8)
+
 
 download_podcast(url)
-clip = get_clip()
-r = json.loads(get_transcript(clip).text)
-text = r['NBest'][0]['Display']
+text = ''
+
+cnt = 0
+for c in range(startTime,endTime,clip_size):
+    cnt += 1
+    clip = get_clip(c, c+clip_size,str(cnt))
+    r = json.loads(get_transcript(clip).text)
+    with open(str(cnt)+".txt", "w") as text_file:
+        text_file.write(str(r))
+    print(r['DisplayText'])
+    text += r['DisplayText']
 
